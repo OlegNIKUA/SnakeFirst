fc0b34c71fdd115b00d2afe1cf020baed04cb28d
OlegNIKUA <o.yrml@mail.ru>
2016-03-16 21:32:27 +0200

    add About form

--- SnakeFirst/About.cs
+++ SnakeFirst/About.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace SnakeFirst
@@ -14,6 +8,11 @@ namespace SnakeFirst
         public About()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
